1a690e30ae8c43147da4e058320eaaf8a52ae08b
Aloyce CodeCraftsman <125455306+Allous-pun@users.noreply.github.com>
2024-04-07 17:46:28 +0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output:{
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+        manualChunks(id) {
+            if(id.includes('node_modules')){
+              return 'vendor';
+            }
+        },
+      },
+    },
+   }
+})
